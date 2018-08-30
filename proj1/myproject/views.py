7096a13c6dfad846950d5377d8fc44ded9650ee7
Edson Yamamoto <Edson.Yamamoto@flex.com>
2018-08-30 09:33:42 -0300

    Visualização de posts

--- proj1/myproject/views.py
+++ proj1/myproject/views.py
@@ -14,7 +14,3 @@ def board_topics(request, pk):
     except Board.DoesNotExist:
         raise Http404
     return render(request, 'topics.html', {'board': board})
-
-def topic_posts(request, pk, topic_pk):
-    topic = get_object_or_404(Topic, board__pk=pk, pk=topic_pk)
-    return render(request, 'topic_posts.html', {'topic': topic})
